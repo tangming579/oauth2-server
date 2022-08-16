f8d2aa76983a065ba45bfa2dac664d5504c95d70
汤明 <infly123@sina.com>
2022-08-16 17:46:51 +0800

    jwks

---
+++ auth-server/src/main/java/com/tm/auth/config/JwkSetEndpoint.java
@@ -0,0 +1,31 @@
+package com.tm.auth.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Map;
+
+/**
+ * @author tangming
+ * @date 2022/8/16
+ */
+@FrameworkEndpoint
+class JwkSetEndpoint {
+
+    @Resource
+    private KeyPair keyPair;
+
+    @GetMapping("/.well-known/jwks.json")
+    @ResponseBody
+    public Map<String, Object> getKey() {
+        RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
+        RSAKey key = new RSAKey.Builder(publicKey).build();
+        return new JWKSet(key).toJSONObject();
+    }
+}
